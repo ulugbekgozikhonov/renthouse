46911496232fe0bb613b3ef2a297a17dd46bf30d
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2026-02-12 21:13:44 +0500

    install redis

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,7 +1,11 @@
 from aiogram import Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
 
+from database import redis
 from routes.commands import router as commands_router
 
-dp = Dispatcher()
+storage = RedisStorage(redis)
+
+dp = Dispatcher(storage=storage)
 
 dp.include_router(commands_router)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from bot import bot
+from database import engine, redis
 from dispatcher import dp
 
 
@@ -8,8 +9,14 @@ def on_startup():
     print("Bot is started...")
 
 
+async def on_shutdown():
+    await engine.dispose()
+    await redis.aclose()
+
+
 async def main():
     dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     await dp.start_polling(bot)  # type: ignore
 
 
