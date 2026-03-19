5d9798193cf2e92618de381e48348341acf21f49
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2026-03-19 14:02:10 +0500

    bug fix

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -5,7 +5,7 @@ from aiogram.utils.i18n.middleware import FSMI18nMiddleware
 from database import redis
 from i18n import i18n
 from messages import MessageServiceMiddleware
-from routes.commands import router as commands_router
+from routes.user import router as user_router
 
 storage = RedisStorage(redis)
 
@@ -13,4 +13,4 @@ dp = Dispatcher(storage=storage)
 
 dp.message.outer_middleware(FSMI18nMiddleware(i18n))
 dp.message.outer_middleware(MessageServiceMiddleware())
-dp.include_router(commands_router)
+dp.include_router(user_router)
